cbd642efded16fba82c21b4ef58ff87a55cac525
mi-wit <artakserkses.mw@gmail.com>
2022-06-21 21:38:28 +0200

    fix remove example text

--- src/app/text-editor/text-editor.component.ts
+++ src/app/text-editor/text-editor.component.ts
@@ -10,30 +10,30 @@ import { defer, from, Subject, takeUntil } from 'rxjs';
 })
 export class TextEditorComponent implements OnInit, OnDestroy {
 
-  plText: string = `Warta Zawiercie klub piłkarski
-z Zawiercia założony w 1921 roku
-z inicjatywy Seweryna Gębarskiego 
-oraz Władysława Millera po tym,
-jak w Zawierciu Anglicy rozpropagowali piłkę nożną.
-Rozgrywki ligowe zespół rozpoczął w 1922 roku w klasie C,
-w 1927 roku awansował do klasy B, a dwa lata później do klasy A.
-W latach 1930 oraz 1932 Warta Zawiercie zdobywała mistrzostwo okręgu kieleckiego klasy A, uzyskując prawo do gry w barażach o Ligę.
-W obu sezonach klub odpadał w pierwszej rundzie baraży,
-zajmując ostatnie miejsce w tabeli.
-W trakcie II wojny światowej zespół zawiesił działalność,
-a po wojnie został reaktywowany z inicjatywy przedwojennego zawodnika klubu,
-Mariana Merty. W latach 70. i 80.
-Warta Zawiercie grała na czwartym i piątym poziomie rozgrywek.
-W 1980`
-  enText: string = `This is a text hyphenation library,
-based on Franklin M. Liang's hyphenation algorithm.
-In core of the algorithm lies a set of hyphenation patterns.
-They are extracted from hand-hyphenated dictionaries.
-Patterns for this library were taken from ctan.org and ported to Javascript.`;
+//   plText: string = `Warta Zawiercie klub piłkarski
+// z Zawiercia założony w 1921 roku
+// z inicjatywy Seweryna Gębarskiego 
+// oraz Władysława Millera po tym,
+// jak w Zawierciu Anglicy rozpropagowali piłkę nożną.
+// Rozgrywki ligowe zespół rozpoczął w 1922 roku w klasie C,
+// w 1927 roku awansował do klasy B, a dwa lata później do klasy A.
+// W latach 1930 oraz 1932 Warta Zawiercie zdobywała mistrzostwo okręgu kieleckiego klasy A, uzyskując prawo do gry w barażach o Ligę.
+// W obu sezonach klub odpadał w pierwszej rundzie baraży,
+// zajmując ostatnie miejsce w tabeli.
+// W trakcie II wojny światowej zespół zawiesił działalność,
+// a po wojnie został reaktywowany z inicjatywy przedwojennego zawodnika klubu,
+// Mariana Merty. W latach 70. i 80.
+// Warta Zawiercie grała na czwartym i piątym poziomie rozgrywek.
+// W 1980`
+//   enText: string = `This is a text hyphenation library,
+// based on Franklin M. Liang's hyphenation algorithm.
+// In core of the algorithm lies a set of hyphenation patterns.
+// They are extracted from hand-hyphenated dictionaries.
+// Patterns for this library were taken from ctan.org and ported to Javascript.`;
   lines: string[] = [];
   numberOfSyllables: number[] = [];
 
-  text = new FormControl(this.plText);
+  text = new FormControl('');
   spellcheck: boolean = false;
   destroy$: Subject<boolean> = new Subject<boolean>();
   constructor() { }
